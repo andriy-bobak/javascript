92ef506d875ae90d39bc675c3b16dd535a2ab302
Andriy <andrewbobak79@gmail.com>
2018-03-25 14:07:20 +0000

    Message

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-alert('Hello World!'); // виведення результату у випадаючому вікні
-document.write('Hello JS'); // виведення результату на сторінці у браузері
-console.log('Hello JS'); // вивведення результату у консолі
+// alert('Hello World!'); // виведення результату у випадаючому вікні
+// document.write('Hello JS'); // виведення результату на сторінці у браузері
+// console.log('Hello JS'); // вивведення результату у консолі
 
 
 
-let productTitle = 'Mazda RX-8';
-let productPrice = 40000 + 20;
-let isAvailable = true;
+// let productTitle = 'Mazda RX-8';
+// let productPrice = 40000 + 20;
+// let isAvailable = true;
 
-document.write(productTitle);
-document.write('<br>');
-document.write(productPrice);
-document.write('<br>');
-document.write(isAvailable);
-document.write('<br>');
-document.write('New price:');
-document.write('<br>');
-productPrice = productPrice * 0.1;
-document.write(productPrice);
-document.write('<br>');
-let productColor; // undefined
-document.write('Product Color');
-document.write('<br>');
-document.write(productColor);
-productColor = null;
-document.write('<br>');
-document.write('Product Color');
-document.write('<br>');
-document.write(productColor);
+// document.write(productTitle);
+// document.write('<br>');
+// document.write(productPrice);
+// document.write('<br>');
+// document.write(isAvailable);
+// document.write('<br>');
+// document.write('New price:');
+// document.write('<br>');
+// productPrice = productPrice * 0.1;
+// document.write(productPrice);
+// document.write('<br>');
+// let productColor; // undefined
+// document.write('Product Color');
+// document.write('<br>');
+// document.write(productColor);
+// productColor = null;
+// document.write('<br>');
+// document.write('Product Color');
+// document.write('<br>');
+// document.write(productColor);
 
-let obj = {}; // let - змінна всередині блоку
+// let obj = {}; // let - змінна всередині блоку
 
 
-let sym = Symbol(); // let - змінна всередині блоку
+// let sym = Symbol(); // let - змінна всередині блоку
 
-// Number = 0, 1, 1.5 1.5e10 NaN Infinity
+// // Number = 0, 1, 1.5 1.5e10 NaN Infinity
 
-console.log(Math.sqrt(4));
-console.log(Math.sqrt(-4));
-console.log(-4/0);
-// Arithmetic operators +, -, *, /, %
+// console.log(Math.sqrt(4));
+// console.log(Math.sqrt(-4));
+// console.log(-4/0);
+// // Arithmetic operators +, -, *, /, %
 
-const msg = 'Hello ' + ' World';
-console.log(msg);
+// const msg = 'Hello ' + ' World';
+// console.log(msg);
 
-const userName = prompt('Enter your name');
-console.log('Hello, ' + userName);
+// const userName = prompt('Enter your name');
+// console.log('Hello, ' + userName);
 
-const userAge = parseFloat(prompt('Enter your age'));
-console.log('Your age is ' + userAge);
-const newAge = userAge + 1;
-console.log('Next year you will be ' + newAge);
+// const userAge = parseFloat(prompt('Enter your age'));
+// console.log('Your age is ' + userAge);
+// const newAge = userAge + 1;
+// console.log('Next year you will be ' + newAge);
 
-// > < == === != !== >= <= && || !
-if (userAge >= 18) {
-    console.log('Adult');
-} else {
-    console.log('Child');
-}
+// // > < == === != !== >= <= && || !
+// if (userAge >= 18) {
+//     console.log('Adult');
+// } else {
+//     console.log('Child');
+// }
 
 const someResult = addThree(10, 20, 30);
 console.log(someResult);
